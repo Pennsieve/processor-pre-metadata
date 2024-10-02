c02ad65c266d764fe2037e1323990c1981906d12
John Frommeyer <johnfr@seas.upenn.edu>
2024-10-02 14:03:22 -0400

    Move data type types into their own package

--- client/models/instance/property.go
+++ client/models/instance/property.go
@@ -4,30 +4,8 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"github.com/pennsieve/processor-pre-metadata/client/models/datatypes"
 )
-
-type SimpleType string
-
-const StringType SimpleType = "String"
-const LongType SimpleType = "Long"
-const DoubleType SimpleType = "Double"
-const BooleanType SimpleType = "Boolean"
-const DateType SimpleType = "Date"
-
-type ComplexType string
-
-const ArrayType ComplexType = "array"
-
-type ArrayDataType struct {
-	Type  ComplexType `json:"type"`
-	Items ItemsType   `json:"items"`
-}
-
-type ItemsType struct {
-	Type   SimpleType `json:"type"`
-	Format string     `json:"format,omitempty"`
-	Unit   string     `json:"unit,omitempty"`
-}
 
 type Property struct {
 	ConceptTitle bool `json:"conceptTitle"`
@@ -42,9 +20,9 @@ type Property struct {
 }
 
 func (p Property) DecodeDataType() (any, error) {
-	var simpleType SimpleType
+	var simpleType datatypes.SimpleType
 	if err := json.Unmarshal(p.DataType, &simpleType); err != nil {
-		var arrayType ArrayDataType
+		var arrayType datatypes.ArrayDataType
 		if arrErr := json.Unmarshal(p.DataType, &arrayType); arrErr != nil {
 			return nil, fmt.Errorf("data type %s is not simple or array: %w", p.DataType, errors.Join(err, arrErr))
 		}
@@ -59,13 +37,13 @@ func (p Property) LongValue() (int64, error) {
 		return 0, err
 	}
 	switch dt := dataType.(type) {
-	case SimpleType:
-		if dt == LongType {
+	case datatypes.SimpleType:
+		if dt == datatypes.LongType {
 			return int64(p.Value.(float64)), nil
 		} else {
 			return 0, fmt.Errorf("data type is not Long: %s", dt)
 		}
-	case ArrayDataType:
+	case datatypes.ArrayDataType:
 		return 0, fmt.Errorf("data type is not Long: %T", dt)
 	default:
 		return 0, fmt.Errorf("unknown dataType: %T", dt)
@@ -79,8 +57,8 @@ func (p Property) ArrayValue() (any, error) {
 		return nil, err
 	}
 	switch dt := dataType.(type) {
-	case ArrayDataType:
-		if dt.Type == ArrayType && dt.Items.Type == LongType {
+	case datatypes.ArrayDataType:
+		if dt.Type == datatypes.ArrayType && dt.Items.Type == datatypes.LongType {
 			var longs []int64
 			if p.Value == nil {
 				return longs, nil
@@ -89,18 +67,18 @@ func (p Property) ArrayValue() (any, error) {
 				longs = append(longs, int64(l.(float64)))
 			}
 			return longs, nil
-		} else if dt.Type == ArrayType && dt.Items.Type == StringType {
+		} else if dt.Type == datatypes.ArrayType && dt.Items.Type == datatypes.StringType {
 			return convertArray[string](p.Value), nil
-		} else if dt.Type == ArrayType && dt.Items.Type == DoubleType {
+		} else if dt.Type == datatypes.ArrayType && dt.Items.Type == datatypes.DoubleType {
 			return convertArray[float64](p.Value), nil
-		} else if dt.Type == ArrayType && dt.Items.Type == BooleanType {
+		} else if dt.Type == datatypes.ArrayType && dt.Items.Type == datatypes.BooleanType {
 			return convertArray[bool](p.Value), nil
-		} else if dt.Type == ArrayType && dt.Items.Type == DateType {
+		} else if dt.Type == datatypes.ArrayType && dt.Items.Type == datatypes.DateType {
 			return convertArray[string](p.Value), nil
 		} else {
 			return nil, fmt.Errorf("data type is not array of Longs: %s of %s", dt.Type, dt.Items.Type)
 		}
-	case SimpleType:
+	case datatypes.SimpleType:
 		return nil, fmt.Errorf("data type is not array: %T", dt)
 	default:
 		return nil, fmt.Errorf("unknown dataType: %T", dt)
